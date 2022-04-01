ecfc2f7f0ab0e48d31209d999527c6cbd8801c4e
Vsevolod Karnaukhov <karnaukhov333@gmail.com>
2022-04-01 11:41:45 +0300

    code reuse

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,37 @@
 <template>
-    hello
-    <div id="app" >
-        <ul v-if="response">
-            <li v-for ="user in response">
-                {{user.name}}
-            </li>
-        </ul>
-
-    </div>
+  <div id="app">
+    <select v-model="selectedTab">
+      <option value="techcrunch">TechCrunch</option>
+      <option value="starwars">StarWars</option>
+    </select>
+    <hr />
+    <TechCrunch v-if="selectedTab === 'techcrunch'" />
+    <StarWars v-if="selectedTab === 'starwars'" />
+  </div>
 </template>
 
 <script>
-    import {getCurrentInstance} from 'vue';//библиотека во vue возвращает this текущего компонента
-    /*
-    async function loadResponse(){
-        const users = await fetch('https://jsonplaceholder.typicode.com/users').then(
-            r => r.json()
-        );
-        return {users};
-    }*/
-    async function loadResponse(){
-        const users = await fetch('https://jsonplaceholder.typicode.com/users').then(
-            r => r.json()
-        );
-        return users;
-    }
-    export default {
-        name: "App",
-        data(){
-            return {
-                //users: null
-                response: null
-            }
-        },
-        /*
-        async created(){
-            this.users = await fetch('https://jsonplaceholder.typicode.com/users').then(
-                r => r.json()
-            );
-            console.log('after')
-        },*/
-        /* другой пример */
+import TechCrunch from "./components/TechCrunch.vue";
+import StarWars from "./components/StarWars.vue";
 
-        async created(){
-            console.log('before');
-            console.log(getCurrentInstance());
-            this.response = await loadResponse();
-            console.log('after');
-            console.log(getCurrentInstance());
-        },
-        /* другой пример */
-        /*
-        created(){
-            console.log(getCurrentInstance());
-            console.log('before');
-            loadResponse().then( (r)=>{
-                this.response = r;
-            } );
-            console.log(getCurrentInstance());
-            console.log('after');
+export default {
+  name: "App",
+  components: {
+    TechCrunch,
+    StarWars,
+  },
+  data() {
+    return {
+      selectedTab: "techcrunch",
+    };
+  },
 
-        },*/
-
-
-        methods: {
-
-        },
-    }
+  watch: {
+    selectedTab() {
+      window.location.hash = "";
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style></style>
---
+++ src/App_async_hook.vue
@@ -0,0 +1,74 @@
+<template>
+    hello
+    <div id="app" >
+        <ul v-if="response">
+            <li v-for ="user in response">
+                {{user.name}}
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+    import {getCurrentInstance} from 'vue';//библиотека во vue возвращает this текущего компонента
+    /*
+    async function loadResponse(){
+        const users = await fetch('https://jsonplaceholder.typicode.com/users').then(
+            r => r.json()
+        );
+        return {users};
+    }*/
+    async function loadResponse(){
+        const users = await fetch('https://jsonplaceholder.typicode.com/users').then(
+            r => r.json()
+        );
+        return users;
+    }
+    export default {
+        name: "App",
+        data(){
+            return {
+                //users: null
+                response: null
+            }
+        },
+        /*
+        async created(){
+            this.users = await fetch('https://jsonplaceholder.typicode.com/users').then(
+                r => r.json()
+            );
+            console.log('after')
+        },*/
+        /* другой пример */
+
+        async created(){
+            console.log('before');
+            console.log(getCurrentInstance());
+            this.response = await loadResponse();
+            console.log('after');
+            console.log(getCurrentInstance());
+        },
+        /* другой пример */
+        /*
+        created(){
+            console.log(getCurrentInstance());
+            console.log('before');
+            loadResponse().then( (r)=>{
+                this.response = r;
+            } );
+            console.log(getCurrentInstance());
+            console.log('after');
+
+        },*/
+
+
+        methods: {
+
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
