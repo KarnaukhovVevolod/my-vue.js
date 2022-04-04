d2b74103fb65242a2070238b0c2deccf25556dc8
Vsevolod Karnaukhov <karnaukhov333@gmail.com>
2022-04-04 09:28:21 +0300

    add file test2

---
+++ src/test2.vue
@@ -0,0 +1,14 @@
+<template>
+    new file for development
+    use amount commit
+</template>
+
+<script>
+    export default {
+        name: "test2"
+    }
+</script>
+
+<style scoped>
+
+</style>
