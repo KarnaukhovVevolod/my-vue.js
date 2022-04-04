b26d4f3be37929d3cd068c96622381cd8ad8edab
Vsevolod Karnaukhov <karnaukhov333@gmail.com>
2022-04-04 09:50:30 +0300

    commit 1 for revert

--- src/test2.vue
+++ src/test2.vue
@@ -1,6 +1,11 @@
 <template>
     new file for development
     use amount commit
+
+    add commit 1 добавили коммит для reverta ...добавляем изменения к коммиту 1 через rebase при этом
+    ветка уже запушена в мастер
+
+
 </template>
 
 <script>
